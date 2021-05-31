b4da625e1b88a1853f88abc24a9177af5220e37a
Irfan Rizvi <sirizvi27@gmail.com>
2021-05-31 19:28:44 +0530

    commit 31-5-2021

--- src/components/Sign-Up/SignUp.js
+++ src/components/Sign-Up/SignUp.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import {Card, Button, In, AppBar, Toolbar, IconButton, Typography} from '@material-ui/core';
+import './SignUp.scss';
+import {Card, Button, In, AppBar, Toolbar, IconButton, Typography, Container, CardContent} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import {Link} from 'react-router-dom';
 
 function SignUp (props) {
 
+    // const Container = {
+    //     backgroundColor: 'yellow',
+    //     width: '100%'
+    // }
+
     return(
 
+        <>
         <AppBar>
             <Toolbar>
-                <IconButton>
+                {/* <IconButton>
                     <MenuIcon />
-                </IconButton>
+                </IconButton> */}
 
                 <Typography variant="h6" >
                      <Link to="/">AppName</Link>
                 </Typography>
             </Toolbar>
         </AppBar>
+
+        <Container maxWidth='sm' >
+
+                <Card xs={12} className="card">
+                    <CardContent className="">
+                        
+                    </CardContent>
+                </Card>
+
+        </Container>
+
+        </>        
        
     )
 
